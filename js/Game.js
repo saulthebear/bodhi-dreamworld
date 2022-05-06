4eb5d9dd02a5f85acc53184af25a50b074fb2c60
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-06 16:09:48 -0600

    Add GameWorld which holds the size of the world

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,7 @@
 export class Game {
   constructor() {
     this.color = this.#randomColor()
+    this.world = new GameWorld(250, 300)
   }
 
   update(timeStep) {
@@ -13,5 +14,16 @@ export class Game {
     const blue = Math.floor(Math.random() * 256)
     const alpha = Math.random()
     return `rgba(${red}, ${green}, ${blue}, ${alpha})`
+  }
+}
+
+class GameWorld {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+  }
+
+  get aspectRatio() {
+    return this.width / this.height
   }
 }
