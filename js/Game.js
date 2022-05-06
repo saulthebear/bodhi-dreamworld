8945924c9dd38a0b723f488c81430eed8a1d5588
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-06 17:55:55 -0600

    Add world edge detection and gravity
    * Player velocity added
    * World gravity added
    * World update function added
    * Player update function added
    * Player initial position passed to constructor

--- js/Game.js
+++ js/Game.js
@@ -6,6 +6,8 @@ export class Game {
 
   update(timeStep) {
     this.color = this.#randomColor()
+
+    this.world.update()
   }
 
   #randomColor() {
@@ -21,20 +23,70 @@ class GameWorld {
   constructor() {
     this.width = 160
     this.height = 192
-    this.player = new Player()
+
+    this.player = new Player(16, 32)
+
+    this.gravity = 3
   }
 
   get aspectRatio() {
     return this.width / this.height
   }
+
+  collideWithWorldEdge(object) {
+    const objectLeft = object.x
+    const objectRight = object.x + object.width
+    const objectTop = object.y
+    const objectBottom = object.y + object.height
+
+    if (objectLeft < 0) {
+      // Object beyond left of world
+      // Place object against left of world and stop its leftward velocity
+      object.x = 0
+      object.xVelocity = 0
+    } else if (objectRight > this.width) {
+      // Object beyond right of world
+      // Place object against right of world and stop its rightward velocity
+      object.x = this.width - object.width
+      object.xVelocity = 0
+    }
+
+    if (objectTop < 0) {
+      // Object beyond top of world
+      // Place object against top of world and stop its upwards velocity
+      object.y = 0
+      object.yVelocity = 0
+    } else if (objectBottom > this.height) {
+      // Object beyond bottom of world
+      // Place object on bottom of world and stop its downwards velocity
+      object.y = this.height - object.height
+      object.yVelocity = 0
+    }
+  }
+
+  update() {
+    this.player.yVelocity += this.gravity
+    this.player.update()
+    this.collideWithWorldEdge(this.player)
+  }
 }
 
 class Player {
-  constructor() {
+  constructor(x, y) {
     this.color = "white"
+
     this.height = 16
     this.width = 16
-    this.x = 5
-    this.y = 5
+
+    this.x = x
+    this.y = y
+
+    this.xVelocity = 0
+    this.yVelocity = 0
+  }
+
+  update() {
+    this.y += this.yVelocity
+    this.x += this.xVelocity
   }
 }
