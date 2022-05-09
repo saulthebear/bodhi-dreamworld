69cc04d61e99d19acfa4fba1a3c0757f7527047c
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-09 12:05:34 -0600

    Represent levels as strings
    * This representation is both human understandable and machine parsable
    * Makes editing levels a lot simpler
    * GameWorld size is also dynamically calculated from this string
    * Currently only parses platforms

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,12 @@
 import { Platform } from "./Platform.js"
 import { Player } from "./Player.js"
+import { level1String } from "./levels.js"
 
 export class Game {
   constructor() {
-    this.world = new GameWorld(250, 300)
+    const blockSize = 8
+    const level1 = new Level(level1String, blockSize)
+    this.world = new GameWorld(level1)
   }
 
   update(timeStep) {
@@ -12,21 +15,19 @@ export class Game {
 }
 
 class GameWorld {
-  constructor() {
-    this.width = 160
-    this.height = 192
+  constructor(level) {
+    this.width = level.width
+    this.height = level.height
+    this.platforms = level.platforms
 
     this.backgroundColor = "black"
 
-    this.player = new Player(16, 32)
+    this.player = new Player(0, 168)
 
     // Downward velocity added every tick
     this.gravity = 3
     // Multiplier applied to velocity every tick
     this.friction = 0.75
-
-    this.platforms = []
-    this.#constructLevel()
   }
 
   get aspectRatio() {
@@ -78,28 +79,147 @@ class GameWorld {
     this.platforms.forEach((platform) => platform.applyColliders(this.player))
   }
 
-  #addPlatform(
-    x,
-    y,
-    width,
-    height,
-    collisionDirections = { top: true, right: true, bottom: true, left: true }
-  ) {
-    this.platforms.push(new Platform(x, y, width, height, collisionDirections))
+  // #addPlatform(
+  //   x,
+  //   y,
+  //   width,
+  //   height,
+  //   collisionDirections = { top: true, right: true, bottom: true, left: true }
+  // ) {
+  //   this.platforms.push(new Platform(x, y, width, height, collisionDirections))
+  // }
+
+  // #constructLevel() {
+  // // floor #1
+  // this.#addPlatform(0, 208, 160, 8, { top: true })
+
+  // // #2
+  // this.#addPlatform(81, 192, 16, 16)
+
+  // // #3 & #4
+  // this.#addPlatform(120, 192, 40, 16, { left: true, top: true })
+  // this.#addPlatform(136, 176, 24, 16, { left: true, top: true })
+
+  // // #5 & #6
+  // this.#addPlatform(56, 160, 56, 8)
+  // this.#addPlatform(56, 144, 32, 16)
+
+  // // #7
+  // this.#addPlatform(0, 120, 32, 8)
+
+  // // #8
+  // this.#addPlatform(56, 104, 32, 8)
+
+  // // #9
+  // this.#addPlatform(112, 96, 48, 8)
+
+  // // #10
+  // this.#addPlatform(0, 72, 48, 8)
+
+  // // #11
+  // this.#addPlatform(56, 72, 16, 8)
+  // // #12
+  // this.#addPlatform(144, 64, 16, 8)
+  // // #13
+  // this.#addPlatform(96, 56, 32, 8)
+  // // #14
+  // this.#addPlatform(72, 32, 24, 8)
+  // // #15
+  // this.#addPlatform(0, 40, 48, 8)
+  // }
+}
+
+class Level {
+  #platforms
+  #player
+  #widthInBlocks
+  #heightInBlocks
+  #blockSize
+
+  constructor(levelString, blockSize) {
+    const rowStrings = this.#splitString(levelString)
+    this.#widthInBlocks = rowStrings[0].length - 2
+    this.#heightInBlocks = rowStrings.length - 2
+    this.#blockSize = blockSize
+
+    this.#platforms = this.#createPlatforms(this.#parseLevelString(rowStrings))
+    // console.log(this.#platforms)
   }
 
-  #constructLevel() {
-    // floor
-    this.#addPlatform(0, 180, 160, 8, { top: true })
+  get platforms() {
+    return this.#platforms
+  }
 
-    this.#addPlatform(81, 168, 16, 16)
+  get width() {
+    return this.#blockSize * this.#widthInBlocks
+  }
 
-    this.#addPlatform(120, 168, 40, 16, { left: true, top: true })
-    this.#addPlatform(136, 152, 24, 16, { left: true, top: true })
+  get height() {
+    return this.#blockSize * this.#heightInBlocks
+  }
 
-    this.#addPlatform(56, 136, 56, 8)
-    this.#addPlatform(0, 88, 32, 8)
+  // Splits the string into rows and trims empty rows
+  #splitString(levelString) {
+    return levelString.split("\n").filter((row) => row.length > 0)
+  }
 
-    this.#addPlatform(56, 120, 32, 16)
+  #parseLevelString(rowStrings) {
+    const rowsOfPlatforms = []
+
+    for (let i = 1; i < rowStrings.length - 1; i++) {
+      const rowString = rowStrings[i]
+
+      // Holds all the start and end indices of platforms on this row
+      let rowPlatforms = []
+
+      let platformStarted = false
+
+      for (let j = 1; j < rowString.length; j++) {
+        const block = rowString[j]
+        const colIndex = j - 1
+
+        // Platform start
+        if (!platformStarted && block === "=") {
+          platformStarted = true
+          rowPlatforms.push({ start: colIndex })
+        }
+
+        // Platform end
+        if (platformStarted && block !== "=") {
+          const lastPlatform = rowPlatforms[rowPlatforms.length - 1]
+          lastPlatform.end = colIndex - 1
+          platformStarted = false
+        }
+      }
+      rowsOfPlatforms.push(rowPlatforms)
+    }
+    console.log(rowsOfPlatforms)
+    return rowsOfPlatforms
+  }
+
+  #createPlatforms(rowsOfPlatforms) {
+    const platforms = []
+    const collisions = { top: true, right: true, bottom: true, left: true }
+
+    for (let i = 0; i < rowsOfPlatforms.length; i++) {
+      const row = rowsOfPlatforms[i]
+      if (row.length === 0) continue
+
+      const y = i * this.#blockSize
+      row.forEach((platformInfo) => {
+        const x = platformInfo.start * this.#blockSize
+        const widthInBlocks = platformInfo.end - platformInfo.start + 1
+        const width = widthInBlocks * this.#blockSize
+        const platform = new Platform(x, y, width, this.#blockSize, collisions)
+        // console.log(platform)
+        // console.log(
+        //   `x: ${x} y: ${y} start: ${platformInfo.start} end: ${platformInfo.end}`
+        // )
+        platforms.push(platform)
+      })
+    }
+    return platforms
   }
 }
+
+// const levelObj = new Level(level1, 8)
